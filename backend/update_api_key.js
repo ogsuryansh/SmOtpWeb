038d86c21f3bac11f956a12950e845027205d92b
vishalbhaideveloper <vishalbhaideveloper@gmail.com>
2026-08-09 22:39:43 +0530

    Change default provider to sastaotp.com

--- backend/update_api_key.js
+++ backend/update_api_key.js
@@ -25,10 +25,10 @@ async function run() {
     );
     console.log('API Key updated in database to: ' + newKey);
 
-    // Ensure URL is set to 247otp
+    // Ensure URL is set to sastaotp
     await Setting.findOneAndUpdate(
       { key: 'otpProviderUrl' },
-      { value: 'https://api.247otp.com/stubs/handler_api.php' },
+      { value: 'https://sastaotp.com/stubs/handler_api.php' },
       { upsert: true }
     );
 
